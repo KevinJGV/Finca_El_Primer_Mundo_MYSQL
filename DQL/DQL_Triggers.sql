1392c56451a0d6c3718e13ef44ae138ed64b15d4
Kevin Gonzalez <kejogo.dev@outlook.com>
2024-10-21 18:05:41 -0500

    feat: realizados 7 triggers

--- DQL/DQL_Triggers.sql
+++ DQL/DQL_Triggers.sql
@@ -106,60 +106,136 @@ BEGIN
     -- CODE
 END//
 
--- 14. Ajustar el saldo de un cliente al eliminar una venta.
+-- 14. Reajustar el stock de recursos de tipo semilla, insumo quimico, riego, material de construccion, energia si tiene un estado diferente a 'activo'.
 
-CREATE TRIGGER nombretrigger
-AFTER|BEFORE INSERT|UPDATE|DELETE ON nombretabla FOR EACH ROW
+CREATE TRIGGER ReajustesStockInventarioTipo
+BEFORE UPDATE ON Recursos FOR EACH ROW
 BEGIN
-    -- CODE
+    DECLARE proceso_nombre VARCHAR(50) DEFAULT 'ReajustesStockInventarioTipo';
+    DECLARE tabla_nombre VARCHAR(50) DEFAULT 'Recursos';
+    DECLARE Detalle TEXT DEFAULT 'Stock reajustado según estado.';
+    DECLARE Tipo VARCHAR(50);
+    
+    IF NEW.ID_Estado <> 1 AND ObtenerTipoRecurso(NEW.ID_Tipo_Recurso) COLLATE utf8mb4_unicode_ci IN ('Semilla', 'Insumo químico', 'Riego', 'Material de construcción', 'Energía') THEN
+        SET NEW.Stock = 0;
+    END IF;
+    INSERT INTO Logs (Tipo_Actividad,Nombre_Actividad,Fecha,Usuario_Ejecutor,Detalles,Tabla_Afectada,ID_Referencia) VALUES
+    ('TRIGGER',proceso_nombre,NOW(),USER(),Detalle,tabla_nombre,OLD.ID);
 END//
 
 -- 15. Registrar la fecha de última actividad de un cliente.
 
-CREATE TRIGGER nombretrigger
-AFTER|BEFORE INSERT|UPDATE|DELETE ON nombretabla FOR EACH ROW
+CREATE TRIGGER UltimaCompraCliente
+AFTER INSERT ON Ventas FOR EACH ROW
 BEGIN
-    -- CODE
+    DECLARE proceso_nombre VARCHAR(50) DEFAULT 'UltimaCompraCliente';
+    DECLARE tabla_nombre VARCHAR(50) DEFAULT 'Ventas';
+    DECLARE Detalle TEXT DEFAULT CONCAT('ID Cliente: ', NEW.ID_Cliente);
+    INSERT INTO Logs (Tipo_Actividad,Nombre_Actividad,Fecha,Usuario_Ejecutor,Detalles,Tabla_Afectada,ID_Referencia) VALUES
+    ('TRIGGER',proceso_nombre,NOW(),USER(),Detalle,tabla_nombre,NEW.ID);
 END//
 
 -- 16. Validar que el nombre del producto no esté vacío.
 
-CREATE TRIGGER nombretrigger
-AFTER|BEFORE INSERT|UPDATE|DELETE ON nombretabla FOR EACH ROW
+CREATE TRIGGER ProductoNombreNoVacioInsert
+BEFORE INSERT ON Productos FOR EACH ROW
 BEGIN
-    -- CODE
+    IF LENGTH(NEW.Nombre) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: No se puede insertar un nombre Vacío en Detalles_Ventas';
+    END IF;
+END//
+
+CREATE TRIGGER ProductoNombreNoVacioUpdate
+BEFORE UPDATE ON Productos FOR EACH ROW
+BEGIN
+    IF LENGTH(NEW.Nombre) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: No se puede actualizar a un nombre Vacío en Detalles_Ventas';
+    END IF;
 END//
 
 -- 17. Registrar el cambio de estado de un cliente.
 
-CREATE TRIGGER nombretrigger
-AFTER|BEFORE INSERT|UPDATE|DELETE ON nombretabla FOR EACH ROW
+CREATE TRIGGER RegistrarNuevoEstadoCliente
+AFTER UPDATE ON Clientes FOR EACH ROW
 BEGIN
-    -- CODE
+    DECLARE proceso_nombre VARCHAR(50) DEFAULT 'RegistrarNuevoEstadoCliente';
+    DECLARE tabla_nombre VARCHAR(50) DEFAULT 'Clientes';
+    DECLARE Detalle TEXT DEFAULT CONCAT('Nuevo Estado: ', NEW.ID_Estado);
+    IF OLD.ID_Estado <> NEW.ID_Estado THEN
+        INSERT INTO Logs (Tipo_Actividad,Nombre_Actividad,Fecha,Usuario_Ejecutor,Detalles,Tabla_Afectada,ID_Referencia) VALUES
+        ('TRIGGER',proceso_nombre,NOW(),USER(),Detalle,tabla_nombre,OLD.ID);
+    END IF;
 END//
 
--- 18. Ajustar el saldo del cliente al actualizar su información.
+-- 18. Calcular el subtotal de un detalle de venta automáticamente.
 
-CREATE TRIGGER nombretrigger
-AFTER|BEFORE INSERT|UPDATE|DELETE ON nombretabla FOR EACH ROW
+CREATE TRIGGER SubtotalCalculado
+BEFORE INSERT ON Detalles_Ventas FOR EACH ROW
 BEGIN
-    -- CODE
+    DECLARE proceso_nombre VARCHAR(50) DEFAULT 'SubtotalCalculado';
+    DECLARE tabla_nombre VARCHAR(50) DEFAULT 'Detalles_Ventas';
+    DECLARE Detalle TEXT DEFAULT 'Subtotal Calculado';
+    SET @Precio = 0;
+
+    SELECT
+        Valor INTO @Precio
+    FROM
+        Productos
+    WHERE
+        ID = NEW.ID_Producto;
+
+    SET NEW.Subtotal = @Precio * NEW.Cantidad;
+
+    INSERT INTO Logs (Tipo_Actividad,Nombre_Actividad,Fecha,Usuario_Ejecutor,Detalles,Tabla_Afectada,ID_Referencia) VALUES
+    ('TRIGGER',proceso_nombre,NOW(),USER(),Detalle,tabla_nombre,NEW.ID);
 END//
 
 -- 19. Prevenir la inserción de un detalle de venta si la cantidad es negativa.
 
-CREATE TRIGGER nombretrigger
-AFTER|BEFORE INSERT|UPDATE|DELETE ON nombretabla FOR EACH ROW
+CREATE TRIGGER AnularDetalle_VentaCantidadNegativa
+BEFORE INSERT ON Detalles_Ventas 
+FOR EACH ROW
 BEGIN
-    -- CODE
+    IF NEW.Cantidad <= 0 THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Error: No se puede insertar una cantidad menor o igual a cero en Detalles_Ventas';
+    END IF;
 END//
 
 -- 20. Registrar cambios en la información del empleado.
 
-CREATE TRIGGER nombretrigger
-AFTER|BEFORE INSERT|UPDATE|DELETE ON nombretabla FOR EACH ROW
+CREATE TRIGGER RegistrarCambioEmpleado
+AFTER UPDATE ON Empleados FOR EACH ROW
 BEGIN
-    -- CODE
+    DECLARE proceso_nombre VARCHAR(50) DEFAULT 'RegistrarCambioEmpleado';
+    DECLARE tabla_nombre VARCHAR(50) DEFAULT 'Empleados';
+    DECLARE Detalle TEXT DEFAULT 'Actualizacion en: ';
+    DECLARE Separador VARCHAR(2) DEFAULT ', ';
+
+    IF OLD.Nombre <> NEW.Nombre THEN
+        SET Detalle = CONCAT(Detalle, 'Nombre - ', NEW.Nombre, Separador);
+    END IF;
+
+    IF OLD.Apellido <> NEW.Apellido THEN
+        SET Detalle = CONCAT(Detalle, 'Apellido - ', NEW.Apellido, Separador);
+    END IF;
+
+    IF OLD.Salario <> NEW.Salario THEN
+        SET Detalle = CONCAT(Detalle, 'Salario - ', NEW.Salario, Separador);
+    END IF;
+
+    IF OLD.Fecha_Contratacion <> NEW.Fecha_Contratacion THEN
+        SET Detalle = CONCAT(Detalle, 'Fecha_Contratacion - ', NEW.Fecha_Contratacion, Separador);
+    END IF;
+
+    IF LENGTH(Detalle) > 0 THEN
+        SET Detalle = LEFT(Detalle, LENGTH(Detalle) - LENGTH(Separador));
+    END IF;
+    
+    INSERT INTO Logs (Tipo_Actividad,Nombre_Actividad,Fecha,Usuario_Ejecutor,Detalles,Tabla_Afectada,ID_Referencia) VALUES
+    ('TRIGGER',proceso_nombre,NOW(),USER(),Detalle,tabla_nombre, NEW.ID);
 END//
 
 DELIMITER ;
