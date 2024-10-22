764d9e37aa5028b11d1d6481712f0046e8e0592e
JavierEAcevedoN <je.acevedonoguera2021@gmail.com>
2024-10-22 16:22:13 -0500

    feat: :sparkles: Se agrego el formato de las 100 consultas
    By @JavierEAcevedoN

--- DQL/DQL_Select.sql
+++ DQL/DQL_Select.sql
@@ -1,1 +1,401 @@
 USE Finca_El_Primer_Mundo;
+
+-- 1
+
+SELECT * FROM
+
+-- 2
+
+SELECT * FROM
+
+-- 3
+
+SELECT * FROM
+
+-- 4
+
+SELECT * FROM
+
+-- 5
+
+SELECT * FROM
+
+-- 6
+
+SELECT * FROM
+
+-- 7
+
+SELECT * FROM
+
+-- 8
+
+SELECT * FROM
+
+-- 9
+
+SELECT * FROM
+
+-- 10
+
+SELECT * FROM
+
+-- 11
+
+SELECT * FROM
+
+-- 12
+
+SELECT * FROM
+
+-- 13
+
+SELECT * FROM
+
+-- 14
+
+SELECT * FROM
+
+-- 15
+
+SELECT * FROM
+
+-- 16
+
+SELECT * FROM
+
+-- 17
+
+SELECT * FROM
+
+-- 18
+
+SELECT * FROM
+
+-- 19
+
+SELECT * FROM
+
+-- 20
+
+SELECT * FROM
+
+-- 21
+
+SELECT * FROM
+
+-- 22
+
+SELECT * FROM
+
+-- 23
+
+SELECT * FROM
+
+-- 24
+
+SELECT * FROM
+
+-- 25
+
+SELECT * FROM
+
+-- 26
+
+SELECT * FROM
+
+-- 27
+
+SELECT * FROM
+
+-- 28
+
+SELECT * FROM
+
+-- 29
+
+SELECT * FROM
+
+-- 30
+
+SELECT * FROM
+
+-- 31
+
+SELECT * FROM
+
+-- 32
+
+SELECT * FROM
+
+-- 33
+
+SELECT * FROM
+
+-- 34
+
+SELECT * FROM
+
+-- 35
+
+SELECT * FROM
+
+-- 36
+
+SELECT * FROM
+
+-- 37
+
+SELECT * FROM
+
+-- 38
+
+SELECT * FROM
+
+-- 39
+
+SELECT * FROM
+
+-- 40
+
+SELECT * FROM
+
+-- 41
+
+SELECT * FROM
+
+-- 42
+
+SELECT * FROM
+
+-- 43
+
+SELECT * FROM
+
+-- 44
+
+SELECT * FROM
+
+-- 45
+
+SELECT * FROM
+
+-- 46
+
+SELECT * FROM
+
+-- 47
+
+SELECT * FROM
+
+-- 48
+
+SELECT * FROM
+
+-- 49
+
+SELECT * FROM
+
+-- 50
+
+SELECT * FROM
+
+-- 51
+
+SELECT * FROM
+
+-- 52
+
+SELECT * FROM
+
+-- 53
+
+SELECT * FROM
+
+-- 54
+
+SELECT * FROM
+
+-- 55
+
+SELECT * FROM
+
+-- 56
+
+SELECT * FROM
+
+-- 57
+
+SELECT * FROM
+
+-- 58
+
+SELECT * FROM
+
+-- 59
+
+SELECT * FROM
+
+-- 60
+
+SELECT * FROM
+
+-- 61
+
+SELECT * FROM
+
+-- 62
+
+SELECT * FROM
+
+-- 63
+
+SELECT * FROM
+
+-- 64
+
+SELECT * FROM
+
+-- 65
+
+SELECT * FROM
+
+-- 66
+
+SELECT * FROM
+
+-- 67
+
+SELECT * FROM
+
+-- 68
+
+SELECT * FROM
+
+-- 69
+
+SELECT * FROM
+
+-- 70
+
+SELECT * FROM
+
+-- 71
+
+SELECT * FROM
+
+-- 72
+
+SELECT * FROM
+
+-- 73
+
+SELECT * FROM
+
+-- 74
+
+SELECT * FROM
+
+-- 75
+
+SELECT * FROM
+
+-- 76
+
+SELECT * FROM
+
+-- 77
+
+SELECT * FROM
+
+-- 78
+
+SELECT * FROM
+
+-- 79
+
+SELECT * FROM
+
+-- 80
+
+SELECT * FROM
+
+-- 81
+
+SELECT * FROM
+
+-- 82
+
+SELECT * FROM
+
+-- 83
+
+SELECT * FROM
+
+-- 84
+
+SELECT * FROM
+
+-- 85
+
+SELECT * FROM
+
+-- 86
+
+SELECT * FROM
+
+-- 87
+
+SELECT * FROM
+
+-- 88
+
+SELECT * FROM
+
+-- 89
+
+SELECT * FROM
+
+-- 90
+
+SELECT * FROM
+
+-- 91
+
+SELECT * FROM
+
+-- 92
+
+SELECT * FROM
+
+-- 93
+
+SELECT * FROM
+
+-- 94
+
+SELECT * FROM
+
+-- 95
+
+SELECT * FROM
+
+-- 96
+
+SELECT * FROM
+
+-- 97
+
+SELECT * FROM
+
+-- 98
+
+SELECT * FROM
+
+-- 99
+
+SELECT * FROM
+
+-- 100
+
+SELECT * FROM
